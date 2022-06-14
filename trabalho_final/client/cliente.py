8a84d536f27b593e49c91879b096c636aa0b61fd
Heitor <heitorventura21@gmail.com>
2022-06-14 13:40:58 -0300

    organização e coisas a fazer

--- trabalho_final/client/cliente.py
+++ trabalho_final/client/cliente.py
@@ -1,5 +1,5 @@
-
 import socket
+import codecs
 
 HOST = "localhost"
 PORT = 1234
@@ -7,9 +7,9 @@ PORT = 1234
 def main():
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world!")
+    s.sendall(bytes("Olá José!", "utf-8"))
     data = s.recv(1024)
-    print("Received", repr(data))
+    print("Received", codecs.decode(data, "utf-8"))
 
 if __name__ == '__main__':
   main()
