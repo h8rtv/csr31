95534cb1e5707845150ab7c9e0d2b9f078c66cd6
Heitor <heitorventura21@gmail.com>
2022-06-29 14:05:14 -0300

    fix server

--- trabalho_final/app/server/server.py
+++ trabalho_final/app/server/server.py
@@ -4,11 +4,11 @@ from lib.env import get_env
 env = get_env()
 
 def tcp_server(callback):
-  while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-      print("Server started")
-      s.bind((env.get("HOST"), env.get("PORT")))
-      print("Server binded")
+  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("Server started")
+    s.bind((env.get("HOST"), env.get("PORT")))
+    print("Server binded")
+    while True:
       s.listen()
       conn, addr = s.accept()
       with conn:
@@ -19,4 +19,3 @@ def tcp_server(callback):
             break
           callback(data)
       conn.close()
-      s.close()
